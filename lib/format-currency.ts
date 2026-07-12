68fe798817f2e3b3991bfe1293430bf3e3c66765
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-07-12 22:10:18 +0700

    feature: util format currency

---
+++ lib/format-currency.ts
@@ -0,0 +1,15 @@
+export function formatIDR(amount: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(amount);
+}
+
+export function formatUSD(amount: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
