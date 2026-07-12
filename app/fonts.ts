4f996408f49a6a5dd260f318562a27e35f6a9b40
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-07-12 13:05:43 +0700

    feature: setting font

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import { Poppins, Inter } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["500", "600", "700"],
+  variable: "--font-display",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-body",
+});
