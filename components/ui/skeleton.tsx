ca95f5f8152fa94605779c7c924a7a19688d6224
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-07-19 02:08:53 +0700

    feature: skeleton loading

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -4,7 +4,10 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn(
+        "animate-shimmer rounded-md bg-[linear-gradient(90deg,#ECEBE7_25%,#F6F5F2_37%,#ECEBE7_63%)] bg-size-[680px_100%]",
+        className
+      )}
       {...props}
     />
   )
